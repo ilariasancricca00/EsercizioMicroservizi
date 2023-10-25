be356e45f47d469110ab7a368783572b2c45a168
ilariasancricca00 <148978922+ilariasancricca00@users.noreply.github.com>
2023-10-25 15:07:50 +0200

    Add files via upload

---
+++ esercizioMicroservizi/src/test/java/it/gammainnovation/esercizioMicroservizi/EsercizioMicroserviziApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gammainnovation.esercizioMicroservizi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercizioMicroserviziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
